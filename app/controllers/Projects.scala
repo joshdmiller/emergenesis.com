05fb27c70120d95bae6e1fe44e51e8e861a75419
Josh Miller <josh.miller@emergenesis.com>
2012-06-25 15:28:37 -0700

    Added basic projects page

--- app/controllers/Projects.scala
+++ app/controllers/Projects.scala
@@ -5,7 +5,10 @@ import play.api.mvc._
 
 object Projects extends Controller {
   
-  def index = TODO
+  def index = Action {
+    Ok(views.html.projects())
+  }
+  
   def citeplasm = TODO
   def bps = TODO
   def semanticweb = TODO
