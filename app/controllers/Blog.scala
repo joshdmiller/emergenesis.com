2cc324f465e537d0185c1d4c095581b988b196f4
Josh David Miller <josh@joshdmiller.com>
2012-08-17 13:59:15 -0700

    Refactored blog views; added admin controls to blog list/show

--- app/controllers/Blog.scala
+++ app/controllers/Blog.scala
@@ -5,6 +5,7 @@ import play.api.mvc._
 import jp.t2v.lab.play20.auth._
 
 import models._
+import views.html
 
 object Blog extends Controller with Auth with Authentication {
   
@@ -13,17 +14,15 @@ object Blog extends Controller with Auth with Authentication {
     val posts = Post.findAll.skip((num-1)*limit).limit(limit)
     val is_prev = num > 1
     val is_next = Post.count > limit * num
-    val is_auth = maybeUser match {
-        case Some(user) => true
-        case None => false
-    }
-    Ok(views.html.blog(posts, num, is_next, is_prev, is_auth))
+    val is_auth = maybeUser match { case Some(user) => true case None => false }
+    Ok(html.blog.list(posts, num, is_next, is_prev, is_auth))
   }
 
-  def show(slug: String) = Action {
+  def show(slug: String) = optionalUserAction { maybeUser => request =>
     Post.findOneBySlug(slug) match {
         case Some(post) => {
-            Ok(views.html.blogPost(post))
+            val is_auth = maybeUser match { case Some(user) => true case None => false }
+            Ok(html.blog.show(post, is_auth))
         }
 
         case None => {
@@ -40,6 +39,10 @@ object Blog extends Controller with Auth with Authentication {
   //def create = authorizedAction(Administrator) { user => implicit request =>
   //}
   def create = TODO
+  
+  def edit(slug: String) = TODO
+  
+  def delete(slug: String) = TODO
   
   def rss = TODO
   
