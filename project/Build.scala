d4f16941544327cfa425b792351c6f34e1fc207b
Josh David Miller <josh@joshdmiller.com>
2012-08-17 22:46:10 -0700

    Added markdown processing of blog posts

--- project/Build.scala
+++ project/Build.scala
@@ -8,6 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0"
 
     
+    
     val appDependencies = Seq(
       "se.radley" %% "play-plugins-salat" % "1.0.8",
       "jp.t2v" %% "play20.auth" % "0.3-SNAPSHOT"
@@ -17,6 +18,8 @@ object ApplicationBuild extends Build {
       resolvers += "t2v.jp repo" at "http://www.t2v.jp/maven-repo/",
       routesImport += "se.radley.plugin.salat.Binders._",
       templatesImport += "org.bson.types.ObjectId"
+    ) dependsOn(
+      RootProject(uri("git://github.com/tristanjuricek/knockoff.git"))
     )
 
 }
